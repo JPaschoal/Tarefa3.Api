ebc6e799e7e19b0e4a6d86ea250cd4c1c7de2dec
JPaschoal <jhonatanpaschoal@gmail.com>
2024-09-30 18:58:12 -0300

    Initial commit

---
+++ Tarefa3.Api/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tarefa3.Domain.Interfaces.Service;
+
+namespace Tarefa3.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_personService.GetAll());
+        }
+
+    }
+}
---
+++ Tarefa3.Application/Services/PersonService.cs
@@ -0,0 +1,42 @@
+﻿using Tarefa3.Domain.Interfaces.Service;
+using Tarefa3.Domain.Models;
+
+namespace Tarefa3.Application.Services
+{
+    public class PersonService : IPersonService
+    {
+        public async Task<List<Person>> GetAll()
+        {
+            List<Person> listPerson = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Paulo Okimoto",
+                    Age = 30,
+                    Address = new PersonAddress
+                    {
+                        Street = "Teste rua",
+                        Number = 123,
+                        City = "Campinas",
+                        State = "SP"
+                    },
+                    Gender = "??"
+                },
+                new Person
+                {
+                    Name = "John Doe",
+                    Age = 40,
+                    Address = new PersonAddress
+                    {
+                        Street = "Teste rua",
+                        Number = 123,
+                        City = "Bragança Paulista",
+                        State = "SP"
+                    },
+                    Gender = "M" 
+                }
+            };
+            return listPerson;
+        }
+    }
+}
